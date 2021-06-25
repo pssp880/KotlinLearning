dbb39dffdc5f710cadbc4e55518f6d4ad23cada3
shiva <psspshiva@gmail.com>
2021-06-25 08:52:47 +0530

    Added new file ComparisionOperators.kt.kt

---
+++ src/main/kotlin/operators/ComparisionOperators.kt
@@ -0,0 +1,11 @@
+package operators
+
+fun main() {
+    val a = 10
+    val b = 20
+    if (a > b) {
+        print("A is Greater then B")
+    } else {
+        print("B is Greater than A")
+    }
+}
